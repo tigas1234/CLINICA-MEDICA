100298bf5c7a6a26cfd9b5c2b86880037cd15850
tigas1234 <tiagovaladares128@gmail.com>
2025-11-17 16:46:26 -0500

    Define Medico model with name and specialty fields

--- medico.js
+++ medico.js
@@ -1,1 +1,6 @@
-
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const Medico = sequelize.define("Medico", {
+nome: DataTypes.STRING,
+especialidade: DataTypes.STRING});
+module.exports = Medico;
