b05e3d011040c64c9638f3c7205cc1bbb4661539
tigas1234 <tiagovaladares128@gmail.com>
2025-11-17 16:50:55 -0500

    Add medicoRoutes for CRUD operations

---
+++ medicoRoutes.js
@@ -0,0 +1,9 @@
+const express = require("express");
+const controller = require("../controllers/medicoController");
+const router = express.Router();
+router.post("/", controller.criar);
+router.get("/", controller.listar);
+router.get("/:id", controller.buscar);
+router.put("/:id", controller.atualizar);
+router.delete("/:id", controller.deletar);
+module.exports = router;
